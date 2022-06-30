d3be581813a2a5f363f761d017610bf9b77019a1
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2022-06-30 12:08:48 -0500

    cardano-dapp-js: npm package v1.0.0
    This library represents a clean subset of
    (https://github.com/thaddeusdiamond/cardano-nft-mint-frontend)[cardano-nft-mint-frontend]
    to enable web creators to initialize Cardano wallets in their website
    and build fully decentralized dApps.
    
    The wallet integration here follows
    (https://cips.cardano.org/cips/cip30/)[CIP-30] related to Cardano
    dApp-Wallet Web Bridges and therefore supports Nami, Typhon, Gero,
    Flint, and Eternl (see
    (https://www.cardano-caniuse.io/)[Cardano-CanIUse]).
    
    NOTE: We include a sample CSS stylesheet to provide a basic dropdown
    (with hover and highlight behavior) in the UI.
    
    [ Documentation: None yet (README.md forthcoming) ]
    [ Testing: Manual link and deployment locally with cardano-nft-mint-frontend ]

---
+++ src/index.js
@@ -0,0 +1,116 @@
+import Toastify from 'toastify-js';
+
+export class CardanoDApp {
+
+  static #SUPPORTED_WALLETS = {
+    eternl: {
+      cip30name: 'eternl',
+      img: 'https://ccvault.io/icons/favicon-128x128.png'
+    },
+    flint: {
+      cip30name: 'flint',
+      img: 'https://flint-wallet.com/favicon.png'
+    },
+    gero: {
+      cip30name: 'gerowallet',
+      img: 'https://gerowallet.io/assets/img/logo2.ico'
+    },
+    nami: {
+      cip30name: 'nami',
+      img: 'https://namiwallet.io/favicon-32x32.png'
+    },
+    typhon: {
+      cip30name: 'typhoncip30',
+      img: 'https://typhonwallet.io/assets/typhon.svg',
+    },
+    yoroi: {
+      cip30name: 'yoroi',
+      img: 'https://yoroi-wallet.com/assets/favicon.png'
+    }
+  }
+
+  static #toastWalletError(error) {
+    var message = error.toString();
+    if (error.constructor === Object) {
+      message = JSON.stringify(error);
+    }
+    Toastify({text: `Wallet error occurred: ${message}`, duration: 3000}).showToast();
+  }
+
+  static #isWalletSupported(walletName) {
+    const walletCip30Name = CardanoDApp.#SUPPORTED_WALLETS[walletName].cip30name;
+    if (!(("cardano" in window) && (walletCip30Name in window.cardano))) {
+      this.#toastWalletError(`Wallet '${walletName}' not integrated in your browser`);
+      return false;
+    }
+    return true;
+  }
+
+  static #enableWallet(walletName) {
+    const cip30name = CardanoDApp.#SUPPORTED_WALLETS[walletName].cip30name;
+    return window.cardano[cip30name].enable();
+  }
+
+  constructor(containerId) {
+    this.containerId = containerId;
+    this.#buildDropdownDom();
+    this.#configureDropdownListeners();
+  }
+
+  #buildDropdownDom() {
+    var dropdownHtml = `<ul><li><a id="${this.#getBannerEl()}" class="text-reset bordered">Connect Wallet&nbsp;&#9660;</a>`;
+    dropdownHtml += '<ul class="dropdown">';
+    for (var wallet in CardanoDApp.#SUPPORTED_WALLETS) {
+      var img = CardanoDApp.#SUPPORTED_WALLETS[wallet].img;
+      dropdownHtml += `<li class="bordered"><a id="${this.containerId}-${wallet}"><img src="${img}" width=20 height=20 />&nbsp;&nbsp;${wallet}</a></li>`;
+    }
+    dropdownHtml += '</ul></li></ul>';
+    document.querySelector(`#${this.containerId}`).innerHTML = dropdownHtml;
+  }
+
+  #configureDropdownListeners() {
+    for (const wallet in CardanoDApp.#SUPPORTED_WALLETS) {
+      document.querySelector(`#${this.containerId}-${wallet}`).addEventListener("click", e => this.connectWallet(e, wallet));
+    }
+  }
+
+  #getBannerEl() {
+    return `${this.containerId}-header`;
+  }
+
+  connectWallet(e, walletName) {
+    e.preventDefault();
+    if (!CardanoDApp.#isWalletSupported(walletName)) {
+      return;
+    }
+
+    CardanoDApp.#enableWallet(walletName).then(wallet =>
+      wallet.getChangeAddress().then(address => {
+        this.selectedWallet = walletName;
+        this.#displayWallet();
+        Toastify({
+            text: `Successfully connected wallet ${address}!`,
+            duration: 3000
+        }).showToast();
+      })
+    ).catch(CardanoDApp.#toastWalletError);
+  }
+
+  #displayWallet() {
+    if (this.isWalletConnected() && this.containerId) {
+      document.querySelector(`#${this.#getBannerEl()}`).textContent = `Connected to ${this.selectedWallet}!`;
+    }
+  }
+
+  getConnectedWallet() {
+    if (this.selectedWallet === undefined) {
+      throw 'No wallet connected to enable';
+    }
+    return CardanoDApp.#enableWallet(this.selectedWallet);
+  }
+
+  isWalletConnected() {
+    return this.selectedWallet !== undefined;
+  }
+
+}
